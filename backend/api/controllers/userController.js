9730aaa2dab9b392a36709cf31ca18f46d97e95b
Juju Liao <liaojulian@gmail.com>
2019-09-29 20:47:20 -0700

    Added delete and update for user

--- backend/api/controllers/userController.js
+++ backend/api/controllers/userController.js
@@ -1,21 +1,41 @@
 var UserModel = require('../models/userModel');
 
+// Delete a user
+exports.delete_a_user = function (req, res) {
+    var queryUsername = req.params.username;
+    UserModel.findOneAndDelete({username: queryUsername}, function(err, obj) {
+        if (err) return res.json({success: false, error: err});
+        return res.send(obj);
+    });
+};
+
+// Update a user
+exports.update_a_user = function(req, res) {
+    var queryUsername = req.params.username;
+    var body = req.body;
+    UserModel.findOneAndUpdate({username: queryUsername}, body, function(err) {
+        if (err) return res.json({success: false, error: err});
+        return res.json({success: true, user: body});
+    });
+}
+
 // Get all users
 exports.get_all_users = function (req, res) {
     // this api will get a user based on their username
     //if the username exits it will pull from out database
     UserModel.find((err, user) => {
         if (err) return res.json({success: false, error: err});
-        return res.json({ success: true, user: user})
-    })
+        return res.json({ success: true, user: user});
+    });
 };
 
+// Get a user
 exports.get_a_user = function (req, res) {
     var queryUsername = req.params.username;
     UserModel.findOne({username: queryUsername}, function(err, obj) {
         if (err) return res.json({success: false, error: err});
         return res.send(obj);
-    })
+    });
 };
 
 // Create a user
--- backend/api/routes/userRoute.js
+++ backend/api/routes/userRoute.js
@@ -5,4 +5,6 @@ module.exports = function (app) {
     app.route('/users').get(user_controller.get_all_users);
     app.route('/users').post(user_controller.create_a_user);
     app.route('/users/:username').get(user_controller.get_a_user);
+    app.route('/users/:username').put(user_controller.update_a_user);
+    app.route('/users/:username').delete(user_controller.delete_a_user);
 };
