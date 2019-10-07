b6579afc1d758318eaab0baa1871463c0cafc8a2
Anthony Jhong <anthonyjhong@gmail.com>
2019-10-06 18:11:42 -0700

    Updated

--- backend/api/routes/postRouter.js
+++ backend/api/routes/postRouter.js
@@ -2,4 +2,8 @@ module.exports = function (app) {
 
     var user_controller = require('../controllers/postController'); 
 
+    app.route('/posts').get(post_controller.get_all_posts_by_user);
+    app.route('/posts').get(post_controller.get_Post_with_id);
+
+
 };
