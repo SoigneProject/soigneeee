fc2e6ec7a5b6124271eed18bdc5dbd9af2f1438b
Anthony Jhong <anthonyjhong@gmail.com>
2019-10-06 18:10:34 -0700

    Added Get request using ID

--- backend/api/controllers/postController.js
+++ backend/api/controllers/postController.js
@@ -1,1 +1,24 @@
 var UserModel = require('../models/postModel');
+
+// exports.get_all_posts_by_user = function(req, res){
+//     var queryUsername = req.params.username;
+
+//     itemModel.findAllForOneUser({
+//         username: queryUsername
+//     });
+
+// }
+
+exports.get_Post_with_id = function(req, res){
+   // Get a user
+    var queryID = req.params.id;
+    postModel.findOne({
+        username: queryID
+    }, function (err, obj) {
+        if (err) return res.json({
+            success: false,
+            error: err
+        });
+        return res.send(obj);
+    });
+}
