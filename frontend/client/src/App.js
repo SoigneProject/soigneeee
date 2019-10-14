439676b34c6409acb512b9244369e9a51f76e321
Leonel <44385935+LeoDAnda18@users.noreply.github.com>
2019-10-13 20:53:51 -0700

    Add files via upload

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,17 +1,47 @@
 // /client/App.js
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import InputField from './InputField';
+import TopMenu from './TopMenu';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import 'typeface-roboto';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import pic from './kennet.JPG';
 import axios from 'axios';
-
+import Paper from '@material-ui/core/Paper';
+import { createMuiTheme } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+import pic1 from './1.jpg';
+import pic2 from './2.jpg';
+import pic3 from './3.jpeg';
+import pic4 from './4.jpeg';
+import pic5 from './5.jpeg';
+import pic6 from './6.jpeg';
+import Signup from './Signup';
+import logo from './soigne.png';
+  
 class App extends Component {
   // initialize our state
   state = {
     data: [],
-    id: 0,
-    message: null,
-    intervalIsSet: false,
-    idToDelete: null,
-    idToUpdate: null,
-    objectToUpdate: null,
   };
 
   // when component mounts, first thing it does is fetch all existing data in our db
@@ -42,21 +72,23 @@ class App extends Component {
   // our first get method that uses our backend api to
   // fetch data from our data base
   getDataFromDb = () => {
-    fetch('http://localhost:3001/api/getData')
-      .then((data) => data.json())
-      .then((res) => this.setState({ data: res.data }));
+    fetch('http://localhost:6969/users')
+      .then((res) => res.json())
+      .then((data) => this.setState({ data: data }));
   };
 
   // our put method that uses our backend api
   // to create new query into our data base
   putDataToDB = (message) => {
-    let currentIds = this.state.data.map((data) => data.id);
+    const { data } = this.state;
+
+    let currentIds = data.map((entry) => entry.id);
     let idToBeAdded = 0;
     while (currentIds.includes(idToBeAdded)) {
       ++idToBeAdded;
     }
 
-    axios.post('http://localhost:3001/api/putData', {
+    axios.post('http://localhost:6969/api/putData', {
       id: idToBeAdded,
       message: message,
     });
@@ -73,7 +105,7 @@ class App extends Component {
       }
     });
 
-    axios.delete('http://localhost:3001/api/deleteData', {
+    axios.delete('http://localhost:6969/api/deleteData', {
       data: {
         id: objIdToDelete,
       },
@@ -95,26 +127,187 @@ class App extends Component {
       id: objIdToUpdate,
       update: { message: updateToApply },
     });
+  
   };
+
+
 
   // here is our UI
   // it is easy to understand their functions when you
   // see them render into our screen
   render() {
     const { data } = this.state;
+    const theme = createMuiTheme({
+      overrides: {
+        // Style sheet name ⚛️
+        MuiTypography: {
+          // Name of the rule
+          textSecondary: {
+            // Some CSS
+            color: 'gray',
+          },
+        },
+      },
+    });
+
+
+    
+    const inputProps = {
+      step: 300,
+    };
+    const avatarStyle = {
+      alignSelf: 'center',
+      height: 250,
+      width: 250,
+      borderWidth: 1,
+    };
+
+
+    const paperStyle = {
+      padding: 4,
+      display: 'flex',
+      overflow: 'auto',
+      flexDirection: 'column',
+      marginTop: 20,
+    }
+
+    const tableStyle = {
+      minWidth: 20,
+      marginTop: 30,
+    }
+    const tileStyle = {
+
+    }
+
+    const gridStyle = {
+      marginTop: 20,
+
+    }
+    const tileData = [
+        
+        {
+          img: pic3,
+          title: 'Image',
+          author: 'author',
+        },
+        {
+          img: pic4,
+          title: 'Image',
+          author: 'author',
+        },
+        {
+          img: pic5,
+          title: 'Image',
+          author: 'author',
+        },
+        {
+          img: pic3,
+          title: 'Image',
+          author: 'author',
+        },
+       ];
+
+    const rows = [
+      createData(291, 492),
+    ];
+    function createData(followers, following) {
+      return { followers, following };
+    }
+    function AvatarLarge(props){
+      return <Avatar src = {props.src} alt = {props.alt} style = {avatarStyle}></Avatar>;
+    }
+
     return (
       <div>
+    <TopMenu/>
+    
+    <Grid container spacing={3}>
+    <Grid item xs={4}>
+      <div style={paperStyle}>
+  <AvatarLarge alt="pic" src={pic} ></AvatarLarge>
+  <Typography align = 'center' variant="h3" component="h2" gutterBottom>
+  Kristina Leo
+</Typography>
+<Typography color = 'textSecondary' align = 'center' variant="h6" component="h2" gutterBottom>
+  @kristinaleopandas
+</Typography>
+<Table style = {tableStyle} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Followers</TableCell>
+            <TableCell align="center">Following</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.name}>
+              <TableCell align="center">{row.followers}</TableCell>
+              <TableCell align="center">{row.following}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+  </div>
+  
+    </Grid>
+    <Grid item xs={8}>
+    <GridList spacing={1} style = {gridStyle} cellHeight={500} >
+
+    {tileData.map(tile => (
+      <GridListTile style = {tileStyle} key={tile.img}>
+        <img src={tile.img} alt={tile.title} />
+        <GridListTileBar
+          title={tile.title}
+          subtitle={<span>by: {tile.author}</span>}
+          actionIcon={
+            <IconButton aria-label={`info about ${tile.title}`} >
+              <InfoIcon />
+            </IconButton>
+          }
+        />
+      </GridListTile>
+    ))}
+  </GridList>
+  
+    </Grid>
+    
+  
+  </Grid>
+   
+
+      <div style={{ padding: '10px' }}>
+        <button onClick={() => window.location = 'Signup.js'}>
+          SignUp
+        </button>
+      </div>
+
+      <img src = {logo} alt = "Logo" style = {{width: '100px'}}/>
+
+      <InputField/>
+      <TopMenu/>
+
         <ul>
           {data.length <= 0
             ? 'NO DB ENTRIES YET'
-            : data.map((dat) => (
+            : data.userObj.map((entry) => (
                 <li style={{ padding: '10px' }} key={data.message}>
-                  <span style={{ color: 'gray' }}> id: </span> {dat.id} <br />
-                  <span style={{ color: 'gray' }}> data: </span>
-                  {dat.message}
+                  <span style={{ color: 'gray' }}> id: </span> {entry._id} <br />
+                  <span style={{ color: 'gray' }}> username: </span>
+                  {entry.username} <br />
+                  <span style={{ color: 'gray' }}> firstName: </span>
+                  {entry.firstName} <br />
+                  <span style={{ color: 'gray' }}> lastName: </span>
+                  {entry.lastName} <br />
+                  <span style={{ color: 'gray' }}> email: </span>
+                  {entry.emailAddress} <br />
+                  <span style={{ color: 'gray' }}> password: </span>
+                  {entry.password} <br />
+                  <span style={{ color: 'gray' }}> profile_id: </span>
+                  {entry.profile_id} <br />
                 </li>
               ))}
-        </ul>
+        </ul> 
         <div style={{ padding: '10px' }}>
           <input
             type="text"
--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Account from './components/Account';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import Signup from './Signup'
 
-ReactDOM.render( < Account / > , document.getElementById('root'));
+const routing = (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Signup">Signup</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={App} />
+        <Route path="/Signup" component={Signup} />
+      </div>
+    </Router>
+  )
+  ReactDOM.render(routing, document.getElementById('root'))
+//ReactDOM.render( < App / > , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
