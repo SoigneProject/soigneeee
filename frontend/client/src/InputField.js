620ecfe204ae2cd1b516a38212caecf2af2bc35e
Anthony Jhong <anthonyjhong@yahoo.com>
2019-10-15 23:15:08 -0700

    Fixed Searchbar Bug

--- frontend/client/src/InputField.js
+++ frontend/client/src/InputField.js
@@ -3,107 +3,95 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import axios from 'axios';
 import Suggestions from './Suggestions';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/styles';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-      flexWrap: 'wrap',
-      width: '40%', 
-      backgroundColor: '#efefef',
-      marginTop: 6,
-      marginLeft: 10
-    },
-    overrides: {
-        MuiFormLabel: {
-          root: {
-            "&$focused": {
-              color: "tomato",
-              fontWeight: "bold"
-            }
-          }, 
-          
-          focused: {}
-        }
-      },
-    margin: {
-      margin: theme.spacing(1),
-    },
-  }));
 
-  class InputField extends Component {
-    constructor(props) {
-      super(props);
 
-      this.state = {
-        query: '',
-        results: []
-      }
+const useStyles = theme => ({
+    root: {
+      flexWrap: 'wrap',
+      width: '100%', 
+      backgroundColor: '#efefef',
+      marginTop: 6,
+      marginLeft: 10
+    },
+    overrides: {
+        MuiFormLabel: {
+          root: {
+            "&$focused": {
+              color: "tomato",
+              fontWeight: "bold"
+            }
+          }, 
+          
+          focused: {}
+        }
+      },
+    margin: {
+    },
+  });
 
-      this.handleInputChange = this.handleInputChange.bind(this);
-    }
-    
+  class InputField extends Component{
+     constructor(props) {
+            super(props);
+      
+            this.state = {
+              query: '',
+              results: []
+            }
+      
+            this.handleInputChange = this.handleInputChange.bind(this);
+          }
+          
+      
+          getInfo() {
+            axios.get(`http://localhost:6969/users/${this.state.query}`)
+            .then( data  => {
+              console.log("Data: " + data.data.username);
+              if (!data.data.username) return;
+              let newResults = this.state.results;
+              newResults.push(data.data.username);
+              this.setState({
+                results: newResults
+              });
+            });
+          }
+      
+          handleInputChange() {
+            this.setState({
+              query: this.search.value
+            }, () => {
+              if (this.state.query && this.state.query.length > 1) {
+                if (this.state.query.length > 2) {
+                  this.getInfo()
+                }
+              } 
+            })
+          }
+      render(){
+        const { classes } = this.props;
+        return (
+      <div style = {{width: '100%', marginTop: 13,}}>
+        <TextField
+        className={classes.root}
+        label="Search for an Outfit:"
+        variant="outlined"
+        id="mui-theme-provider-outlined-input"
+         inputRef={input => this.search = input}
+          onChange={this.handleInputChange}
+        InputProps={{
+            disableUnderline: true,
+           }}
+        style = {{}}
+      />
+      <Suggestions results={this.state.results} />
+      </div>
 
-    getInfo() {
-      axios.get(`http://localhost:6969/users/${this.state.query}`)
-      .then( data  => {
-        console.log("Data: " + data.data.username);
-        if (!data.data.username) return;
-        let newResults = this.state.results;
-        newResults.push(data.data.username);
-        this.setState({
-          results: newResults
-        });
-      });
-    }
-
-    handleInputChange() {
-      this.setState({
-        query: this.search.value
-      }, () => {
-        if (this.state.query && this.state.query.length > 1) {
-          if (this.state.query.length > 2) {
-            this.getInfo()
-          }
-        } 
-      })
-    }
-      
-    render() {
-      const classes = useStyles;
-      return (
-        <form>
-        <TextField
-          className={classes.root}
-          label="Search for an Outfit:"
-          variant="outlined"
-          id="mui-theme-provider-outlined-input"
-          inputRef={input => this.search = input}
-          onChange={this.handleInputChange}
-          InputProps={{
-              disableUnderline: true,
-          }}
-          style = {{}}
-        />
-        <Suggestions results={this.state.results} />
-        </form>
-      );
-    }
-  }
-
-  export default InputField
-  
-  // export default function FilledTextFields() {
-  //   const classes = useStyles();
-  //   return (
-  //       <TextField
-  //       className={classes.root}
-  //       label="Search for an Outfit:"
-  //       variant="outlined"
-  //       id="mui-theme-provider-outlined-input"
-  //       InputProps={{
-  //           disableUnderline: true,
-  //          }}
-  //       style = {{}}
-  //     />
-
-  // );
-  //   }
+  );
+      }
+  }
+  InputField.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  export default withStyles(useStyles)(InputField);
--- frontend/client/src/TopMenu.js
+++ frontend/client/src/TopMenu.js
@@ -7,100 +7,115 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-
+import { createMuiTheme } from '@material-ui/core/styles';
 import InputField from './InputField';
 import logo from './soigne.png';
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props;
 
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`nav-tabpanel-${index}`}
-      aria-labelledby={`nav-tab-${index}`}
-      {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`nav-tabpanel-${index}`}
+      aria-labelledby={`nav-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
 }
 
 TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
 };
 
 function a11yProps(index) {
-  return {
-    id: `nav-tab-${index}`,
-    'aria-controls': `nav-tabpanel-${index}`,
-  };
+  return {
+    id: `nav-tab-${index}`,
+    'aria-controls': `nav-tabpanel-${index}`,
+  };
 }
 
 function LinkTab(props) {
-  return (
-    <Tab
-      component="a"
-      onClick={event => {
-        event.SignIn();
-      }}
-      {...props}
-    />
-  );
+  return (
+    <Tab
+      component="a"
+      onClick={event => {
+        event.SignIn();
+      }}
+      {...props}
+    />
+  );
 }
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: 'white',
-    textColor: 'black',
-    maxwidth: 500,
-  },
+  root: {
+    flexGrow: 1,
+    backgroundColor: 'white',
+    textColor: 'black',
+    maxwidth: 500,
+  },
 
 }));
 
+const theme = createMuiTheme({
+        overrides: {
+          // Style sheet name ⚛️
+  MuiAppBar: {
+            // Name of the rule
+  colorPrimary: {
+              // Some CSS
+  backgroundColor: 'white',
+            },
+          },
+        },
+      });
+
 export default function NavTabs() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-  return (
-    <div className={classes.root}>
-    <img src = {logo} alt = "Logo" style = {{width: '100px'}}/>
+  return (
+    <div className={classes.root}>
+    <div style = {{display: 'inline-flex',}}>
+    <img src = {logo} alt = "Logo" style = {{width: '16%', height: 76, marginTop: 5,}}/>
 
-    <InputField/>
+    <InputField/>
+    </div>
 
-      <AppBar position="static" elevation={0}>
-        <Tabs
-          variant="fullWidth"
-          value={value}
-          onChange={handleChange}
-          tabItemContainerStyle={{width: '400px'}}
-          aria-label="nav tabs example"
-          
-          TabIndicatorProps={{
-            style: {
-              backgroundColor: "tomato "
+      <AppBar style = {{backgroundColor: 'white',}} position="static" elevation={0}>
+        <Tabs
+          variant="fullWidth"
+          value={value}
+          onChange={handleChange}
+          tabItemContainerStyle={{width: '400px'}}
+          aria-label="nav tabs example"
+          
+          TabIndicatorProps={{
+            style: {
+              backgroundColor: "tomato "
 
-            }
-          }}
-          style = {{backgroundColor: 'white', color: 'black'}}
-        >
-          
-          <LinkTab label="About" href="/drafts" {...a11yProps(0)} />
-          <LinkTab label="Favorites" href="/trash" {...a11yProps(1)} />
-          <LinkTab label="Trending" href="/spam" {...a11yProps(2)} />
-          <LinkTab label="Kristina" href = "/spam" {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      
-    </div>
-  );
+            }
+          }}
+          style = {{backgroundColor: 'white', color: 'black'}}
+        >
+          
+          <LinkTab label="About" href="/drafts" {...a11yProps(0)} />
+          <LinkTab label="Favorites" href="/trash" {...a11yProps(1)} />
+          <LinkTab label="Trending" href="/spam" {...a11yProps(2)} />
+          <LinkTab label="Kristina" href = "/spam" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      
+    </div>
+  );
 }
